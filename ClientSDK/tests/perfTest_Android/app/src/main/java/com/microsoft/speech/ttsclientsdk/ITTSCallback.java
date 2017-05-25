aa57a5d1bbdd7b5bfedb66e576e31cf996f49f33
yangguo <v-guoya@microsoft.com>
2017-05-25 19:50:43 +0800

    remove android test

--- ClientSDK/tests/perfTest_Android/app/src/main/java/com/microsoft/speech/ttsclientsdk/ITTSCallback.java
+++
@@ -1,9 +0,0 @@
-package com.microsoft.speech.ttsclientsdk;
-
-/**
- * Created by v-guoya on 5/5/2017.
- */
-
-public interface ITTSCallback {
-    public int ReceiveWave(long responceHandle, byte[] data, int size);
-}
